088c8250d68658eacc8e559d24044bf3d7e3e6d8
Amir Sharapov <amir@amirfounder.com>
2022-04-17 13:07:12 -0500

    cleanup and stuff

--- http_server/http_server.py
+++ http_server/http_server.py
@@ -1,4 +1,4 @@
-from typing import Dict, Type
+from typing import Dict, List
 
 from flask import Flask, request
 from flask_cors import CORS
@@ -8,14 +8,15 @@ from http_server.exceptions import HttpException, NotAllowedException
 from http_server.http_service import HttpService
 
 
-class HttpController:
-    def __init__(self, method_service_map: Dict[HttpMethods, HttpService]):
+class HttpServerController:
+    def __init__(self, route: str, method_service_map: Dict[str, HttpService]):
+        self.route = route
         self.method_service_map = method_service_map
 
-    def on_request(self):
+    def on_request(self, _request: request):
         try:
-            if service := self.method_service_map.get(request.method):
-                return service.run()
+            if service := self.method_service_map.get(_request.method):
+                return service.run(_request)
             raise NotAllowedException()
         except HttpException as e:
             return e.dict()
@@ -24,20 +25,41 @@ class HttpController:
 class HttpServer:
     def __init__(self, port: int = 8080):
         self.app: Flask = Flask(__name__)
-        self.services: Dict[str, Dict[HttpMethods, HttpService]] = {}
+        self.services: Dict[str, Dict[str, HttpService]] = {}
         self.port = port
+        self.is_service_routing_setup = False
 
         CORS(self.app)
+    
+    def register_services(self, services: List[HttpService]):
+        for service in services:
+            self.register_service(service)
 
-    def register_service(self, service_cls: Type[HttpService], service_params: Dict):
-        service = service_cls(service_params)
-        if (route := service.route) not in self.services:
+    def register_service(self, service: HttpService):
+        route = service.route
+        method = service.method.value
+        
+        is_route_registered = route in self.services
+        
+        if not is_route_registered:
             self.services[route] = {}
-        if (method := service.method) not in self.services[route]:
+        
+        is_method_registered = method in self.services[route]
+        
+        if not is_method_registered:
             self.services[route][method] = service
+            return
+        
+        raise Exception(f'Service already registered under route, method : {route} {method}')
+            
+    def setup_service_routing(self):
+        for route in self.services:
+            route_controller = HttpServerController(route, self.services[route])
+            route_methods = [method.value for method in HttpMethods]
+            self.app.route(route, methods=route_methods)(route_controller.on_request)
+        self.is_service_routing_setup = True
 
     def run(self):
-        for route in self.services:
-            controller = HttpController({m.value: s for m, s in self.services[route].items()})
-            self.app.route(route, methods=[e.value for e in HttpMethods])(controller.on_request)
+        if not self.is_service_routing_setup:
+            self.setup_service_routing()
         self.app.run(port=self.port)
--- http_server/http_service.py
+++ http_server/http_service.py
@@ -1,14 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, Dict
 
 from http_server.http_methods import HttpMethods
 
 
-class HttpService(ABC):
-    @abstractmethod
-    def __init__(self, route: str, method: HttpMethods):
-        self.route = route
+T = TypeVar('T')
+
+
+class AbstractHttpServiceAdapter(Generic[T], ABC):
+    def __init__(self, route: str, method: HttpMethods, adaptee: T):
         self.method = method
+        self.route = route
+        self.adaptee = adaptee
 
     @abstractmethod
-    def run(self):
+    def run(self, params: Dict[str]):
         pass
