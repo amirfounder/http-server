f1e1d7610b3afc029423e05e2b8ab292419375c7
Amir Sharapov <amir@amirfounder.com>
2022-04-17 02:04:46 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='http-server',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Flask==2.1.1',
+        'Flask-Cors==3.0.10'
+    ]
+)
